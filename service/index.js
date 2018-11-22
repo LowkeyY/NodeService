b436c8c7198779d0fb802fbbf6376b4f8b0352b8
LowkeyY <742940804@qq.com>
2018-11-22 15:16:27 +0800

    123

--- service/index.js
+++ service/index.js
@@ -4,16 +4,13 @@ var cheerio = require('cheerio')
 var app = express();
 
 app.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", req.headers.host);
+    res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By", ' 3.2.1')
     res.header("Content-Type", "application/json;charset=utf-8");
-    if (req.method == 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
+    next();
 });
 
 app.get('/', function (req, res) {

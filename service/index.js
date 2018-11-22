b7901163528cd6a360bb3737d2a11429908d52da
LowkeyY <742940804@qq.com>
2018-11-22 15:22:54 +0800

    123

--- service/index.js
+++ service/index.js
@@ -4,7 +4,7 @@ var cheerio = require('cheerio')
 var app = express();
 
 app.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", req.headers.host);
+    res.header("Access-Control-Allow-Origin",  req.headers.origin);
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
